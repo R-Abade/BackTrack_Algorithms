ed5831278a30ca93fe43410064a5fccc2e5603d8
R-Abade <raphaelabade10@gmail.com>
2025-07-29 21:24:04 -0300

    horse ride done

--- horseRide.kt
+++ horseRide.kt
@@ -13,22 +13,27 @@ class horseRide(val sizeX: Int, val sizeY: Int) {
 
         tabuleiro[y][x] = i
 
+        // Condição de parada: se visitamos todas as casas
+        if (i == sizeX * sizeY) {
+            return true
+        }
+
         do {
             nextX = x + moveX[k]
             nextY = y + moveY[k]
 
             if (nextX in 0 until sizeX && nextY in 0 until sizeY && tabuleiro[nextY][nextX] == 0) {
-                if (i < sizeX * sizeY) {
-                    nice = tryNextMove(i + 1, nextX, nextY)
-                    if (!nice) {
-                        tabuleiro[nextY][nextX] = 0
-                    }
-                } else {
-                    nice = true
+                nice = tryNextMove(i + 1, nextX, nextY)
+                if (!nice) {
+                    tabuleiro[nextY][nextX] = 0
                 }
             }
             k++
         } while (!nice && k < 8)
+
+        if (!nice) {
+            tabuleiro[y][x] = 0
+        }
 
         return nice
     }
@@ -38,4 +43,27 @@ class horseRide(val sizeX: Int, val sizeY: Int) {
             println(linha.joinToString(" ") { it.toString().padStart(2, '0') })
         }
     }
+
+    fun startGame(){
+        print("Digite um número de entrada horizontal do tabuleiro: ")
+        var row = readLine()!!.toInt()
+        print("Digite um número de entrada vertical do tabuleiro: ")
+        var column = readLine()!!.toInt()
+
+        if (column in 0 until sizeX && row in 0 until sizeY) {
+            if (tryNextMove(1, column, row)) {
+                println("Solução encontrada:")
+            } else {
+                println("Não há solução a partir dessa posição.")
+            }
+            printTabuleiro()
+        } else {
+            println("Coordenadas fora do tabuleiro.")
+        }
+    }
+}
+
+fun main(){
+    var game = horseRide(8,8)
+    game.startGame()
 }
